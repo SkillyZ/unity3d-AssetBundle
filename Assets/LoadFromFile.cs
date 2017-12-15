3928b28ea06b52ea1f4158bd01d9506741696ad8
周闽强 <zhouminqiang@kuainiugroup.com>
2017-12-15 19:31:07 +0800

    asset bundle load asset

---
+++ Assets/LoadFromFile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadFromFile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AssetBundle ab = AssetBundle.LoadFromFile("AssetBundles/scene/wall.unity3d");
+        GameObject wallPrefab = ab.LoadAsset<GameObject>("Wall");
+        Instantiate(wallPrefab);
+
+        //Object[] objs = ab.LoadAllAssets();
+        //foreach (Object o in objs)
+        //{
+        //    Instantiate(o);
+        //}
+    }
+	
+}
